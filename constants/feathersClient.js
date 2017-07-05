22102e96155b6b69f406fc36f17abe939547423c
olln3 <johnnyyellow@gmail.com>
2017-07-05 16:52:23 +0800

    Use isomorphic-fetch

--- constants/feathersClient.js
+++ constants/feathersClient.js
@@ -1,6 +1,6 @@
 import feathers from 'feathers/client';
 import rest from 'feathers-rest/client';
-import fetch from 'node-fetch';
+import fetch from 'isomorphic-fetch';
 
 const baseUrl = 'http://119.29.68.179/xhapi';
 const feathersClient = feathers().configure(rest(baseUrl).fetch(fetch));
